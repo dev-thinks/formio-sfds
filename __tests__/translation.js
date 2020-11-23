0140d22ebb0fc4c9eda6e1b5436767a4d2606683
Shawn Allen <shawnbot@users.noreply.github.com>
2020-11-23 13:43:39 -0800

    add basic test for empty select option translation

--- __tests__/translation.js
+++ __tests__/translation.js
@@ -1,5 +1,7 @@
 /* eslint-env jest */
-import { createForm, destroyForm } from '../lib/test-helpers'
+import { createForm, destroyForm, sleep } from '../lib/test-helpers'
+
+const { Event } = window
 
 // FIXME: we import the built bundle because we aren't (yet)
 // telling jest to process our JS with rollup, which is where
@@ -33,6 +35,42 @@ describe('field translations', () => {
 
     const label = form.element.querySelector('label:not(.control-label--hidden)')
     expect(label.textContent.trim()).toEqual('Nombre')
+
+    destroyForm(form)
+  })
+})
+
+describe('select options', () => {
+  it('translates labels by value', async () => {
+    const form = await createForm({
+      components: [
+        {
+          type: 'select',
+          key: 'select',
+          values: [
+            { value: 'hi', label: 'Hi' }
+          ]
+        }
+      ]
+    }, {
+      language: 'es',
+      i18n: {
+        es: {
+          'select.values.none': 'Nada'
+        }
+      }
+    })
+
+    const select = form.element.querySelector('select')
+    select.focus()
+    select.dispatchEvent(new Event('change'))
+    await sleep(100)
+
+    expect(form.i18next.language).toEqual('es')
+    expect(form.t('select.values.none')).toEqual('Nada')
+
+    const option = form.element.querySelector('option')
+    expect(option.textContent.trim()).toEqual('Nada')
 
     destroyForm(form)
   })
